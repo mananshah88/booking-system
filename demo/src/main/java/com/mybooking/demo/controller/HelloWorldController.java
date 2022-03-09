3dafaa6b5bd3935e9ae4b59a82d0572cc4f4e942
manan <manan@pedagogy.study>
2022-03-09 21:14:03 +0530

    Removing unused+temp code

--- demo/src/main/java/com/mybooking/demo/controller/HelloWorldController.java
+++ demo/src/main/java/com/mybooking/demo/controller/HelloWorldController.java
@@ -1,6 +1,5 @@
 package com.mybooking.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,34 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mybooking.demo.model.cache.FirstCache;
-import com.mybooking.demo.repository.cache.FirstCacheRepository;
-import com.mybooking.demo.repository.nosql.FirstCollectionRepository;
-import com.mybooking.demo.repository.rdbms.FirstTableRepository;
-
 @RestController
 @RequestMapping(value = "/hello-world")
 public class HelloWorldController {
 
-	@Autowired
-	FirstTableRepository firstTableRepo;
-
-	@Autowired
-	FirstCollectionRepository firstCollectionRepo;
-
-	@Autowired
-	FirstCacheRepository firstCacheRepo;
-
 	@GetMapping
 	@ResponseBody
 	public ResponseEntity<String> hello() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Hello World ");
-		sb.append(firstTableRepo.findAll());
-		sb.append(firstCollectionRepo.findAll());
-		firstCacheRepo.save(new FirstCache("1", "D"));
-		sb.append(firstCacheRepo.findAll());
-		return new ResponseEntity<>(sb.toString(), HttpStatus.OK);
+		return new ResponseEntity<>("Hello World!!", HttpStatus.OK);
 	}
 
 }
