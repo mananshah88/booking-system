ad59714a00f232e4f32edeec61d84da0914e8b0b
manan <manan@pedagogy.study>
2022-03-12 16:24:28 +0530

    Removing unnecessary files and misc changes

--- demo/src/main/java/com/mybooking/demo/model/nosql/CrewMember.java
+++ demo/src/main/java/com/mybooking/demo/model/nosql/CrewMember.java
@@ -86,4 +86,53 @@ public class CrewMember extends BaseModel {
 				+ photoUrl + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((photoUrl == null) ? 0 : photoUrl.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CrewMember other = (CrewMember) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (photoUrl == null) {
+			if (other.photoUrl != null)
+				return false;
+		} else if (!photoUrl.equals(other.photoUrl))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
 }
--- demo/src/main/java/com/mybooking/demo/model/nosql/Movie.java
+++ demo/src/main/java/com/mybooking/demo/model/nosql/Movie.java
@@ -41,6 +41,7 @@ public class Movie extends BaseModel {
 	private String description;
 	private List<String> languages;
 	private List<String> genres;
+	private List<Integer> cities;
 	private List<String> formats;
 
 	/* TimeDuration In minutes */
@@ -171,9 +172,18 @@ public class Movie extends BaseModel {
 		super();
 	}
 
+	@Override
+	public String toString() {
+		return "Movie [id=" + id + ", name=" + name + ", title=" + title + ", thumbnailUrl=" + thumbnailUrl
+				+ ", treasureLink=" + treasureLink + ", trailerLink=" + trailerLink + ", description=" + description
+				+ ", languages=" + languages + ", genres=" + genres + ", cities=" + cities + ", formats=" + formats
+				+ ", timeDuration=" + timeDuration + ", certificate=" + certificate + ", releaseDate=" + releaseDate
+				+ ", team=" + team + "]";
+	}
+
 	public Movie(String id, String name, String title, String thumbnailUrl, String treasureLink, String trailerLink,
-			String description, List<String> languages, List<String> genres, List<String> formats, Integer timeDuration,
-			String certificate, List<CrewMember> team, Date releaseDate) {
+			String description, List<String> languages, List<String> genres, List<Integer> cities, List<String> formats,
+			Integer timeDuration, String certificate, Date releaseDate, List<CrewMember> team) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -184,20 +194,121 @@ public class Movie extends BaseModel {
 		this.description = description;
 		this.languages = languages;
 		this.genres = genres;
+		this.cities = cities;
 		this.formats = formats;
 		this.timeDuration = timeDuration;
 		this.certificate = certificate;
-		this.team = team;
 		this.releaseDate = releaseDate;
+		this.team = team;
 	}
 
 	@Override
-	public String toString() {
-		return "Movie [id=" + id + ", name=" + name + ", title=" + title + ", thumbnailUrl=" + thumbnailUrl
-				+ ", treasureLink=" + treasureLink + ", trailerLink=" + trailerLink + ", description=" + description
-				+ ", languages=" + languages + ", genres=" + genres + ", formats=" + formats + ", timeDuration="
-				+ timeDuration + ", certificate=" + certificate + ", team=" + team + ", releaseDate=" + releaseDate
-				+ "]";
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((certificate == null) ? 0 : certificate.hashCode());
+		result = prime * result + ((cities == null) ? 0 : cities.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((formats == null) ? 0 : formats.hashCode());
+		result = prime * result + ((genres == null) ? 0 : genres.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((languages == null) ? 0 : languages.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((releaseDate == null) ? 0 : releaseDate.hashCode());
+		result = prime * result + ((team == null) ? 0 : team.hashCode());
+		result = prime * result + ((thumbnailUrl == null) ? 0 : thumbnailUrl.hashCode());
+		result = prime * result + ((timeDuration == null) ? 0 : timeDuration.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((trailerLink == null) ? 0 : trailerLink.hashCode());
+		result = prime * result + ((treasureLink == null) ? 0 : treasureLink.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		if (certificate == null) {
+			if (other.certificate != null)
+				return false;
+		} else if (!certificate.equals(other.certificate))
+			return false;
+		if (cities == null) {
+			if (other.cities != null)
+				return false;
+		} else if (!cities.equals(other.cities))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (formats == null) {
+			if (other.formats != null)
+				return false;
+		} else if (!formats.equals(other.formats))
+			return false;
+		if (genres == null) {
+			if (other.genres != null)
+				return false;
+		} else if (!genres.equals(other.genres))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (languages == null) {
+			if (other.languages != null)
+				return false;
+		} else if (!languages.equals(other.languages))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (releaseDate == null) {
+			if (other.releaseDate != null)
+				return false;
+		} else if (!releaseDate.equals(other.releaseDate))
+			return false;
+		if (team == null) {
+			if (other.team != null)
+				return false;
+		} else if (!team.equals(other.team))
+			return false;
+		if (thumbnailUrl == null) {
+			if (other.thumbnailUrl != null)
+				return false;
+		} else if (!thumbnailUrl.equals(other.thumbnailUrl))
+			return false;
+		if (timeDuration == null) {
+			if (other.timeDuration != null)
+				return false;
+		} else if (!timeDuration.equals(other.timeDuration))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (trailerLink == null) {
+			if (other.trailerLink != null)
+				return false;
+		} else if (!trailerLink.equals(other.trailerLink))
+			return false;
+		if (treasureLink == null) {
+			if (other.treasureLink != null)
+				return false;
+		} else if (!treasureLink.equals(other.treasureLink))
+			return false;
+		return true;
 	}
 
 }
